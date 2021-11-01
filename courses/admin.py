aedccf30b4ff8d57a3f9be9f22da743a654a73c3
Rajendra <rajendradanve@gmail.com>
2021-11-01 18:51:58 +0000

    Created models for Courses, MainCategory, Category and Courseschedule in the courses app

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Courses, Category, CourseSchedule, MainCategory
 
 # Register your models here.
+admin.site.register(Courses)
+admin.site.register(Category)
+admin.site.register(CourseSchedule)
+admin.site.register(MainCategory)
--- courses/models.py
+++ courses/models.py
@@ -34,15 +34,33 @@ class Category(models.Model):
 class Courses(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=254)
+    title = models.CharField(max_length=254, null=False, blank=False)
     description = models.TextField()
     prerequisite = models.TextField()
     learning_objectives = models.TextField()
     for_whom = models.TextField()
     number_of_lectures = models.PositiveIntegerField()
     
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    
     date_added = models.DateField(auto_now_add=True)
     time_added = models.TimeField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
     time_updated = models.TimeField(auto_now=True)
     
+    
+    def __str__(self):
+        return self.title
+    
+
+class CourseSchedule(models.Model):
+    course_id = models.ForeignKey('Courses', on_delete=models.CASCADE)
+    course_date = models.DateField()
+    course_time = models.TimeField()
+    course_link = models.CharField(max_length=1024, null=False, blank=False)
+    course_timezone = models.CharField(max_length=100)
+    date_added = models.DateField(auto_now_add=True)
+    time_added = models.TimeField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    time_updated = models.TimeField(auto_now=True)
