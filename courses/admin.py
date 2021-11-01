adb75c1db462cb9dd6186d5bff2aa696158b7de1
Rajendra <rajendradanve@gmail.com>
2021-11-01 19:39:20 +0000

    updated pural name in databases by adding meta class. Also updated display of databases using list_display in admin.py in the Courses App

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
-from .models import Courses, Category, CourseSchedule, MainCategory
+from .models import Course, Category, CourseSchedule, MainCategory
 
 # Register your models here.
-admin.site.register(Courses)
-admin.site.register(Category)
-admin.site.register(CourseSchedule)
-admin.site.register(MainCategory)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'category',
+        'created_by',
+        'price',
+        'rating',
+        'image',
+    )
+    
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'main_category',
+    )
+
+
+class MainCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class CourseScheduleAdmin(admin.ModelAdmin):
+    list_display = (
+        'course_id',
+        'course_date',
+        'course_time',
+        'course_link',
+    )
+    
+    
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(CourseSchedule, CourseScheduleAdmin)
+admin.site.register(MainCategory, MainCategoryAdmin)
--- courses/models.py
+++ courses/models.py
@@ -6,32 +6,38 @@ from django.contrib.auth.models import User
 
 class MainCategory(models.Model):
 
-    main_category_name = models.CharField(max_length=254)
-    main_category_friendly_name = models.CharField(max_length=254,
-                                                   null=True, blank=True)
+    class Meta: 
+        verbose_name_plural = 'Main Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.main_category_name
+        return self.name
 
     def get_friendly_name(self):
-        return self.main_category_friendly_name
+        return self.friendly_name
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=254)
-    category_friendly_name = models.CharField(max_length=254,
+    
+    class Meta: 
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254,
                                               null=True, blank=True)
     main_category = models.ForeignKey('MainCategory', null=True, blank=True,
                                       on_delete=models.SET_NULL)
 
     def __str__(self):
-        return self.category_name
+        return self.name
 
     def get_friendly_name(self):
-        return self.category_friendly_name
+        return self.friendly_name
 
 
-class Courses(models.Model):
+class Course(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=254, null=False, blank=False)
@@ -40,6 +46,8 @@ class Courses(models.Model):
     learning_objectives = models.TextField()
     for_whom = models.TextField()
     number_of_lectures = models.PositiveIntegerField()
+    price = models.PositiveIntegerField(null=False, blank=False)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
@@ -55,7 +63,7 @@ class Courses(models.Model):
     
 
 class CourseSchedule(models.Model):
-    course_id = models.ForeignKey('Courses', on_delete=models.CASCADE)
+    course_id = models.ForeignKey('Course', on_delete=models.CASCADE)
     course_date = models.DateField()
     course_time = models.TimeField()
     course_link = models.CharField(max_length=1024, null=False, blank=False)
