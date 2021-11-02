6c27adc1d742f45eba3cc3f9d3a2d66dc79772f6
Rajendra <rajendradanve@gmail.com>
2021-11-02 16:00:29 +0000

    courses app view updated with new all_courses function. Added urls for courses view

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Course, CourseSchedule
 
 # Create your views here.
+
+
+def all_courses(request):
+    """ A view to show all courses including sorting and search queries"""
+    
+    courses = Course.objects.all()
+    
+    context = {
+        'courses': courses,
+        
+    }
+    
+    return render(request, 'courses/course.html', context)
