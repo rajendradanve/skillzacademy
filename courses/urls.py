43305f0cc3e649da1c48164c17cc2de33fe04bfd
Rajendra <rajendradanve@gmail.com>
2021-11-09 00:40:23 +0000

    course detail page created. Basic data is added to see if getting correct course information from the view. Page UI to be fixed

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_courses, name='courses'),
+    path('<course_id>', views.course_detail, name='course_detail'),
     ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course, CourseSchedule
 
 # Create your views here.
@@ -15,3 +15,19 @@ def all_courses(request):
     }
     
     return render(request, 'courses/courses.html', context)
+
+
+def course_detail(request, course_id):
+    """ A view to show individual course details"""
+    
+    course = get_object_or_404(Course, pk=course_id)
+    course_schedule = CourseSchedule.objects.filter(course_id__pk=course.id)
+    
+
+    context = {
+        'course': course,
+        'course_schedule': course_schedule,
+        
+    }
+    
+    return render(request, 'courses/course_detail.html', context)
