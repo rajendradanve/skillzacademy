2c7c9d2b935f0da1caca877365d9b68abf61f86f
Rajendra <rajendradanve@gmail.com>
2021-11-10 10:29:00 +0000

    added search functionality

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Course, CourseSchedule
 
 # Create your views here.
@@ -6,11 +8,23 @@ from .models import Course, CourseSchedule
 
 def all_courses(request):
     """ A view to show all courses including sorting and search queries"""
-    
     courses = Course.objects.all()
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            print("test")
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter search criteria!")
+                return redirect(reverse('courses'))
+            # Created queries with 'or' condition
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            
+            courses = courses.filter(queries)
     
     context = {
         'courses': courses,
+        'search_term': query,
         
     }
     
