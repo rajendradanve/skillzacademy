8e8cdab874e210136da377318d27b3d313653625
Rajendra <rajendradanve@gmail.com>
2021-11-12 13:28:28 +0000

    added add_to_bag functionality.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,18 @@ def view_bag(request):
     """ A view that renders the bag contents page """
     
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, course_id):
+    """ Add a course to the bag """
+    
+    redirect_url = request.POST.get('redirect_url')
+    
+    bag = request.session.get('bag', [])
+    
+    bag.append(course_id)
+    
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    
+    return redirect(redirect_url)
