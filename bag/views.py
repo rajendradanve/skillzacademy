4639dd22a3a5e347230dbece1750f5b6278cd82a
Rajendra <rajendradanve@gmail.com>
2021-11-15 15:38:28 +0000

    added toast functionality. Not working currently.

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
-
+from django.contrib import messages
+from courses.models import Course
 # Create your views here.
 
 
@@ -12,6 +13,8 @@ def view_bag(request):
 def add_to_bag(request, course_id):
     """ Add a course to the bag """
     
+    course = Course.objects.get(pk=course_id)
+    
     redirect_url = request.POST.get('redirect_url')
     
     bag = request.session.get('bag', [])
@@ -19,18 +22,17 @@ def add_to_bag(request, course_id):
     bag.append(course_id)
     
     request.session['bag'] = bag
-
-    
+    messages.success(request, f'Added {course.title} to your bag')
     return redirect(redirect_url)
 
 
 def remove_course_from_bag(request, course_id):
     """ Adjust the bag view a course to the bag """
-    print(type(course_id))
+    
     bag = request.session.get('bag', [])
     
     bag.remove(course_id)
     
     request.session['bag'] = bag
-    print(bag)
+    
     return redirect(reverse('view_bag',))
