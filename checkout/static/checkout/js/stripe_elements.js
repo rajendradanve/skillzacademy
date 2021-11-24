bd86410d6625cba30a8fff1d515f186fb446e0b3
Rajendra <rajendradanve@gmail.com>
2021-11-24 23:27:53 +0000

    Added basic checkout functionality and tested that it is orking okay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,8 @@
+// Most of this code is taken from butiquo ado project from code institute.
 
-
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
-let client_secret = $('#id_client_secret').text().slice(1,-1);
-let stripe = Stripe(stripe_public_key);
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+let clientSecret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 
 let style = {
@@ -11,7 +11,7 @@ let style = {
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
-        '::placeholder' : {
+        '::placeholder': {
             color: '#aab7c4'
         }
     },
@@ -21,25 +21,60 @@ let style = {
     }
 };
 
-let card = elements.create('card', {style: style});
+let card = elements.create('card', {
+    style: style
+});
 card.mount('#card-element');
 
 // Handle realtime validation erros on the card element
 
-card.addEventListener('change', function(event){
+card.addEventListener('change', function (event) {
     let errorDiv = document.getElementById('card-errors');
 
-    if(event.error){
+    if (event.error) {
         let html = `
         <span class = "icon" role = "alert">
             <i class = "fas fa-times"></i>
             </span>
             <span>${event.error.message}</span>
             `
-        
-        $(errorDiv).html(html);        
-    } else{
-        errorDiv.textContent= '';
+
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
     }
 });
 
+// Handle form submit
+let form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    card.update({
+        'disabled': true
+    });
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function (result) {
+        if (result.error) {
+            let errorDiv = document.getElementById('card-errors');
+            let html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({
+                'disabled': false
+            });
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -24,7 +24,7 @@ def checkout(request):
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY,
     )
-    print(intent)
+    
     order_form = OrderForm()
     
     if not stripe_public_key:
