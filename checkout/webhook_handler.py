3588d0ccdbb7d90a80da8e099fe84c9f68365839
Rajendra <rajendradanve@gmail.com>
2021-12-02 22:59:16 +0000

    webhook error resolved by updating end point in stripe dashboard

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,37 +1,32 @@
-# This code is taken from Boutique Ado project from Code Institute.
 from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """ Handle Stripe webhooks"""
-    
+    """Handle Stripe webhooks"""
+
     def __init__(self, request):
         self.request = request
-        
+
     def handle_event(self, event):
         """
-        Handle a generic/unknown/unexpectd webhook event 
+        Handle a generic/unknown/unexpected webhook event
         """
-        
         return HttpResponse(
-            content=f'Unhandled Webhook received: { event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
-        
+
     def handle_payment_intent_succeeded(self, event):
         """
-        Handle the payment intent succeeded webhook from stripe 
+        Handle the payment_intent.succeeded webhook from Stripe
         """
-        
         return HttpResponse(
-            content=f'Webhook received: { event["type"]}',
+            content=f'Webhook received: {event["type"]}',
             status=200)
-        
+
     def handle_payment_intent_payment_failed(self, event):
         """
-        Handle the payment intent failed webhook from stripe 
+        Handle the payment_intent.payment_failed webhook from Stripe
         """
-        
         return HttpResponse(
-            content=f'payment failed Webhook received: { event["type"]}',
+            content=f'failed Webhook received: {event["type"]}',
             status=200)
-        
