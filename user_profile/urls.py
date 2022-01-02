00abc091dc72a3d70d6f350b756fdafa4663d16f
Rajendra <rajendradanve@gmail.com>
2022-01-02 17:30:28 +0000

    admin page added with 3 links. created add course page

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('profile', views.admin_profile, name='admin'),
+    path('add/course', views.add_course, name='add_course'),
     ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -5,11 +5,16 @@ from django.shortcuts import render
 
 def profile(request):
     """ A view to return the index page"""
-    
+
     return render(request, 'user_profile/profile.html')
 
 
 def admin_profile(request):
     """ A view to return the index page"""
-    
+
     return render(request, 'user_profile/admin.html')
+
+def add_course(request):
+    """ A view to return the index page"""
+
+    return render(request, 'user_profile/add_course.html')
