3c33e17e979c7f6238a1c240fc9336a7fd94540c
Rajendra <rajendradanve@gmail.com>
2022-01-10 20:44:26 +0000

    updated issue with add course category

--- courses/forms.py
+++ courses/forms.py
@@ -1,26 +1,27 @@
 from django import forms
 from .models import Category, MainCategory, Course, CourseSchedule
+from ckeditor.fields import RichTextField
 
 
-class AddCourseForm(forms.ModelForm): 
+class AddCourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ['category', 'title', 'description', 'prerequisite',
                          'learning_objectives', 'for_whom', 'instructor_info',
                          'number_of_lectures', 'price']
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            categories = Category.objects.all()
-            friendly_names_categories = [(c.id, c.get_friendly_name()) for c in categories]
-
-            self.fields['category'].choices = friendly_names_categories
-
-            for field in self.fields.items():
-                field.widget.attrs['class'] = 'border-dark rounded-1'
-                field.widget.attrs['style'] = 'width:100%;'
-
-
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        
+        self.fields['category'].choices = friendly_names
+        
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-dark rounded-1'
+        
+        
+        
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
