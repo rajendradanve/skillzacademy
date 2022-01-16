294021ccbd77a7636bea1bdbde4604b0ca726c92
Rajendra <rajendradanve@gmail.com>
2022-01-16 16:57:10 +0000

    remove secret key and set debug

--- skillzacademy/settings.py
+++ skillzacademy/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#h&+9l5v=7tz2x3n*(pq(co9tsd$resccdemuqz*1xhc!1+htu'
+SECRET_KEY = os.environ.get('SEKRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['skillz-academy.herokuapp.com', 'localhost']
 
